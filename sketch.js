8e3a885865b7af4bf74c4ffb0fd5784af151a70b
arujasaraf <65903153+arujasaraf@users.noreply.github.com>
2020-08-16 16:38:47 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,93 @@
+//Create variables here
+var dog,dogImg
+ var happyDogImg, database, foodS, foodStock;
+ var fedTime,lastFed,foodObj;
+function preload()
+{  
+dogImg=loadImage("images/dogImg.png")
+happyDogImg=loadImage("images/dogImg1.png")
+  //load images here
+}
+
+function setup() {
+  database = firebase.database()
+  createCanvas(1000, 600);
+  Dog = createSprite (300,300,10,10)
+  Dog.addImage(dogImg);
+  foodObj=new Food();
+  foodStock  = database.ref('food');
+  foodStock.on("value",readStock);
+  feed=createButton("Feed the Dog");
+  feed.position(700,95);
+  feed.mousePressed(feedDog);
+
+  addFood=createButton("Add Food");
+  addFood.position(800,95);
+addFood.mousePressed(addFoods)
+
+}
+
+
+function draw() {  
+background(46,139,87)
+//if(keyDown(UP_ARROW))
+//{
+ // writeStock(foodS);
+ //Dog.addImage(happyDogImg);
+fill(255,255,254);
+textSize(15);
+if (lastFed>=12){
+  text("Last Feed :"+lastFed%12+ " PM",350,30);
+}else if(lastFed==0){
+  text("Last Feed : 12 AM",350,30);
+}else {
+  text("Last Feed :"+ lastFed+ " AM",350,30)
+};
+foodObj.display();
+//}
+  drawSprites();
+  fedTime=database.ref ('FeedTime');
+fedTime.on("value",function(data){
+  lastFed=data.val();}) 
+textSize(25)
+        fill("white")
+  text("press up arrow key to feed drago milk!",15,20)
+  //add styles here
+
+}
+function readStock(data)
+{
+  foodS = data.val();
+foodObj.updateFoodStock(foodS);
+}
+/*function writeStock(x)
+{
+  if(x<=0)
+  {
+    x=0;
+  }
+  else{
+    x=x-1;
+  }
+
+
+
+  database.ref('/').update({
+    Food:x
+  })
+}*/
+function feedDog(){
+  Dog.addImage(happyDogImg);
+
+  foodObj.updateFoodStock(foodObj.getFoodStock()-1);
+  database.ref('/').update({
+    Food:foodObj.getFoodStock(),
+    FoodTime:hour()
+  })
+}
+function addFoods(){
+  foodS=foodS+1
+  database.ref('/').update({
+    Food:foodS
+  })
+}
